baf72eec8a3422d9ccdca80e73cd1ed423eba9c6
Samdaman52 <122140631+Samdaman52@users.noreply.github.com>
2023-01-31 15:00:28 -0600

    Delete password.sh

--- password.sh
+++
@@ -1,5 +0,0 @@
-password        [success=1 default=ignore]      pam_unix.so obscure yescrypt minlen=15 minclass=4 remember=3 onerr=fail deny=5 unlock_time=900 
-password        requisite                       pam_deny.so
-password        required                        pam_permit.so
-
-
